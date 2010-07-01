cb45b856965a8ae7ef0f72482c463f307b8bfc8e
Eric Smith <eric@8thlight.com>
2010-06-30 22:28:23 -0500

    Generic cleanup.

--- Classes/PingPongScorerViewController.m
+++ Classes/PingPongScorerViewController.m
@@ -12,7 +12,6 @@
 -(void) scorePlayerOnePoint:(id) sender
 {
   [playerOne score];
-//  [self updateScreen];
 }
 
 -(void) scorePlayerTwoPoint:(id) sender
@@ -21,23 +20,6 @@
   [self updateScreen];
 }
 
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 -(void) viewDidLoad 
 {
   [super viewDidLoad];
@@ -47,6 +29,7 @@
                   context:nil];
 }
 
+
 -(void) observeValueForKeyPath:(NSString *) keyPath
                       ofObject:(id) object
                         change:(NSDictionary *) change
@@ -54,16 +37,6 @@
 {
   [self updateScreen];
 }
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation 
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 -(void) didReceiveMemoryWarning 
 {
@@ -73,7 +46,7 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload 
+-(void) viewDidUnload 
 {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
